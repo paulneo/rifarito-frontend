8655fba6d5dfeb75917d4effee2715ad27a0ac85
Alexei Mamani <alexeim763@gmail.com>
2020-07-19 17:15:45 -0500

    ...

--- src/Components/StepOne/brain/index.js
+++ src/Components/StepOne/brain/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CardGeneral from '../UI/CardGeneral'
 import { Provider } from '../context'
 import ModalComponent from '../UI/ModalComponent'
+import { Form } from 'antd'
+import Context from '../context'
+
 const StepOne = () => {
+  const { form } = useContext(Context)
   return (
     <Provider>
-      <CardGeneral />
+      <Form>
+        <CardGeneral />
+      </Form>
       <ModalComponent></ModalComponent>
     </Provider>
   );
--- src/Components/StepOne/context.js
+++ src/Components/StepOne/context.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import { Form } from 'antd'
 
 const Context = createContext()
 
@@ -7,11 +8,14 @@ export const Provider = ({ children }) => {
   const [buyer, setBuyer] = useState({
     quantity: 1
   })
+  const [ form ] = Form.useForm();
+
   const value = {
     buyer,
     setBuyer,
     modal,
     setModal,
+    form
   };
 
   return <Context.Provider value={value}>{children}</Context.Provider>

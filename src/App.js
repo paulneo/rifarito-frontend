00d5e366b25d9f966618697db4d54affe9fd8b0b
Alexei Mamani <alexeim763@gmail.com>
2020-07-19 15:55:39 -0500

    Added router react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 import RouterApp from './Router/'
 
 function App() {
   return (
-    <div className="Container">
-      <RouterApp />
-    </div>
+    <RouterApp />
   );
 }
 
---
+++ src/Router/Layout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Header, Content, Footer } = Layout;
+
+function App({ children }) {
+  return (
+    <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          {/* <Menu.Item key="1">
+            <Link to='/stepTwo'>Inicio</Link>
+          </Menu.Item> */}
+          <Menu.Item key="2">
+            <Link to='/'>Comprar Rifa</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to='/registerTickets'>Registrar números</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content className="site-layout" style={{ padding: '5px 50px', marginTop: 84 }}>
+        { children }
+      </Content>
+      <Footer style={{ textAlign: 'center' }}></Footer>
+    </Layout>
+  );
+}
+
+export default App;
